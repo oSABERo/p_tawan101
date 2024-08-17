30e2003c4a9ba400af26f4064f26236125bd35e8
beer <surakiat.kongchana@enov8.com>
2024-08-17 15:09:48 +0700

    class user_info

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #python 101
-class Test:
+class user_info:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
+    def info(self):
+        return f'Name: {self.name}, Age: {self.age}'
 
+test = user_info('Beer', 21)
+
+info_string = test.info()
+print(info_string)
